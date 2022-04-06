02f0ac177513ab584879b5fda308424b9631d343
vasanth8888 <vasanthveeda09@gmail.com>
2022-04-07 00:36:31 +0530

    ball

---
+++ src/test/java/org/p123/Balloon.java
@@ -0,0 +1,19 @@
+package org.p123;
+
+public class Balloon {
+	
+	
+	private void hip1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void hip2() {
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
